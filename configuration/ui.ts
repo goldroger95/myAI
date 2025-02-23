48f71124c998959b463995d3c6aef33e753ae421
sagnik mukherjee <sagnikmukherjee01@gmail.com>
2025-02-23 01:04:07 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Ramses is here for you`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Ready to get blown away`;
 export const CLEAR_BUTTON_TEXT: string = `Reset`;
-export const PAGE_TITLE: string = `Ask Huko`;
+export const PAGE_TITLE: string = `Ask Ramses`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
