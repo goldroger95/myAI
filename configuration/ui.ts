7b90fe237d19cd6716e475c1ee210a42604af3ea
sagnik mukherjee <sagnikmukherjee01@gmail.com>
2025-02-23 00:50:42 -0500

    Update ui.ts
    Edited Welcome Message

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `News Bot`;
+export const CHAT_HEADER: string = `Ramses is here for you`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Ready to get blown away`;
 export const CLEAR_BUTTON_TEXT: string = `Reset`;
