cb3766ffd9967a25833bf6ad9cecb0db84470fcb
sagnik mukherjee <sagnikmukherjee01@gmail.com>
2025-02-24 20:00:25 -0500

    Update ui.ts
    Changed Message placeholder
    and a couple of other UI elements

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,8 +1,8 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Ramses is here for you`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Ready to get blown away`;
+export const CHAT_HEADER: string = `Ramses AI`;
+export const MESSAGE_PLACEHOLDER: string = `Hi I'm Ramses, the official mascot of UNC and your buddy to answer any questions. Go ahead and ask me anything about the UNC MBA. Here are a few topics I can help you with 1. MBA Administrative Processes 2. Coursework 3. Housing 4. Restaurants and Social Life `;
+export const FOOTER_MESSAGE: string = `Go Heels!!!`;
 export const CLEAR_BUTTON_TEXT: string = `Reset`;
 export const PAGE_TITLE: string = `Ask Ramses`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
